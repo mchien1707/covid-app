8e9bfd8b9723e03b64f8f53b3d78906de400fc57
MinhChien <mchien1707@gmail.com>
2022-01-26 10:56:54 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <GChart
+      type="GeoChart"
+      :data="chartData"
+      :options="chartOptions"
+      :settings="{ packages: ['corechart', 'map', 'geochart'] }"
+    /> -->
+    <GChart
+      type="GeoChart"
+      :data="chartData"
+      :options="chartOptions"
+      :settings="{ packages: ['geochart'] }"
+    />
+    <!-- <GChart
+      type="PieChart"
+      :data="chartData"
+      :options="chartOptions"
+      :settings="{ packages: ['corechart'] }"
+    /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { GChart } from "vue-google-charts";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "App",
+  data: () => {
+    return {
+      chartData: [["CountryID", "Country", "Total", "Death"]],
+      chartOptions: {
+        chart: {
+          title: "Covid Tracking",
+          subtitle: "",
+        },
+        colorAxis: { colors: ["7bcefa", "ff9002", "f5500c"] },
+      },
+      width: 1000
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    GChart,
+  },
+  created() {
+    this.getData();
+  },
+  methods: {
+    async getData() {
+      const data = await axios.get("https://corona.lmao.ninja/v2/countries"); /*axios.get("https://api.covid19api.com/summary");*/
+      let chartValue = [];
+      data.data.map((item) => {
+        let country = [];
+        country.push(
+          item.countryInfo.iso2,
+          item.country,
+          item.cases,
+          item.deaths
+        );
+        chartValue.push(country);
+      });
+
+      this.chartData = [...this.chartData, ...chartValue];
+    },
+  },
+};
 </script>
 
 <style>
